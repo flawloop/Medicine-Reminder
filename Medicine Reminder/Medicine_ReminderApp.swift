6e219b08b0009eedffebbb9b639d2f1939ab49e1
flawloop <khusravkhamidov@gmail.com>
2022-08-09 17:36:16 +0600

    Initial Commit

---
+++ Medicine Reminder/Medicine_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medicine_ReminderApp.swift
+//  Medicine Reminder
+//
+//  Created by yaaburnee on 09/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Medicine_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
